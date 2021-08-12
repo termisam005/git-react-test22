318c6faf9cb9c4ca1d6b813dbb6230768c87be8f
termisam005 <termi0004@gmail.com>
2021-08-13 01:24:39 +0900

    react-test-0812

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br />
+        <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
