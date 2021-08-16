316cc70c259498401ea92d0381612e22875db96d
termisam005 <termi0004@gmail.com>
2021-08-16 16:58:41 +0900

    commit-0816

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
@@ -10,13 +16,27 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br />
-        <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/movies">
+            <h1>Movie list</h1>
+            <form onSubmit={onSubmit}>
+              <input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br />
+              <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
+              <button type="submit">Login</button>
+            </form>
+          </Route>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
